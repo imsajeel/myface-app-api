f4335f9d9cd569e81148e546f5ad64aca3c8bfd1
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-20 11:51:07 +0500

    /register updated and now able to send data to  both login and users table

--- server.js
+++ server.js
@@ -44,35 +44,36 @@ app.get("/", (req, res) => {
   res.send(database.users);
 });
 
-app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error loging in");
-  }
-});
+app.post("/signin", (req, res) => {});
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
 
-  bcrypt.hash(password, null, null, function (err, hash) {
-    console.log(hash);
-  });
+  const hash = bcrypt.hashSync(password);
 
-  db("users")
-    .returning("*")
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("Unable to register."));
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("Unable to register."));
 });
 
 app.get("/profile/:id", (req, res) => {
