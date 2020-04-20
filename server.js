c595e8bd53b3477b1c13c0ead9aeb6a5db61ab7f
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-20 15:14:28 +0500

    some modifications done

--- server.js
+++ server.js
@@ -29,18 +29,15 @@ app.get("/allusers", (req, res) => {
   allusers.handleAllusers(req, res, db);
 });
 
-app.post("/signin", (req, res) => {
-  signin.handleSignin(req, res, db, bcrypt);
-});
+// with advance way - req and res are in signin.js
+app.post("/signin", signin.handleSignin(db, bcrypt));
 
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
-
 app.get("/profile/:id", (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
-
 app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
